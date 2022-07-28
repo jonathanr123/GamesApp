2e460bde411bbbf28a453b1acab704b29e72d440
Jonathan Sergio Rodriguez Alvarez <joni_la12_elnacio@hotmail.com>
2022-07-27 21:26:24 -0300

    Instalé dependencia iniciales, navgraph inicial, y el bottom navigation

--- app/src/main/java/com/example/gamesapp/MainActivity.kt
+++ app/src/main/java/com/example/gamesapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.gamesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.example.gamesapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavView: BottomNavigationView
+    private lateinit var toolbar: ActionBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        toolbar = supportActionBar!!
+        bottomNavView = binding.navigationView
     }
 }
